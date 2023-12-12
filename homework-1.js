5c6a388e12ca14e328c7d53077aab3d85ec77440
Adham Ismonov <kkirapm@gmail.com>
2023-12-12 18:02:43 +0500

    lecture

---
+++ homework-1.js
@@ -0,0 +1,33 @@
+
+    // function task1(a,b){
+    //     return a + b
+    // }
+    // console.log(task1(2,3));
+
+    // function task2(a) {
+    //     if (a > 0) {
+    //         return"Positive";
+    //       } else if (a < 0) {
+    //         return"Negative";
+    //       }else{
+    //           return "Zero";
+    //       }
+        
+    // }
+    // console.log(task2(0));
+
+    // // task-3
+    // let a = 5;
+
+    //     for (let i = 1; i <= 10; i++) {
+    //         console.log(a +  " * " +  i +  " = "  + a*i);
+            
+    //     }
+
+    // let a =59;
+    // if (a%10 == 9 ) {
+    //     console.log("YES");
+    // }else{
+    //     console.log("NO");
+    // }
+
